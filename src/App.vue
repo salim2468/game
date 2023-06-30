320c19b76c501afbedb586da6a9121d37b638f7c
salim2468 <salimmaharjan2468@gmail.com>
2023-06-30 14:05:18 +0545

    form done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<SignupForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from './components/SignupForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignupForm
   }
 }
 </script>
@@ -19,7 +19,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="container">
+    <form @click.prevent="handleSubmit">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="email"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          v-model="password"
+        />
+      <p class="text-danger small">{{passwordError}}</p>
+      </div>
+      
+      <div class="mb-3">
+        <select
+          v-model="role"
+          style="width:50%; padding:10px; 20px; border-width:2px; border-radius:10px;"
+        >
+          <option value="Web Develper">Web Develper</option>
+          <option value="Backend Develper">Backend Develper</option>
+        </select>
+      </div>
+
+
+<div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Accept Terms and condition</label>
+  </div>
+
+      
+
+      <label class="form-check-label" for="exampleCheck1">Language:</label>
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value="English"
+          v-model="languages"
+        />
+        <label class="form-check-label" for="flexCheckDefault"> English </label>
+      </div>
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value="Nepali"
+          checked
+          v-model="languages"
+        />
+        <label class="form-check-label" for="flexCheckChecked"> Nepali </label>
+      </div>
+
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Skills</label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputPassword1"
+          @keyup="addSkill"
+          v-model="tempSkill"
+        />
+        <div style="display: flex">
+          <div v-for="skill in skills" :key="skill" style="margin: 5px 4px 4px 0px;">
+            <span class="badge text-bg-secondary" style="font-size: 14px; cursor:pointer;" @click="removeSkill(skill)">{{ skill }}</span>
+          </div>
+        </div>
+      </div>
+
+      <button  class="btn btn-outline-secondary">Submit</button>
+    </form>
+  </div>
+  <p>Terms {{ terms }}</p>
+
+  <p>{{ tempSkill }}</p>
+  <p>{{ lang }}</p>
+  
+</template>
+
+<script>
+export default {
+  name: "SignupForm",
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "Web Develper",
+      terms: true,
+      languages: [],
+      tempSkill: "",
+      skills: [],
+      passwordError: "",
+    };
+  },
+  methods: {
+    addSkill(e) {
+      console.log("function called");
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.tempSkill = this.tempSkill.substring(
+            0,
+            this.tempSkill.length - 1
+          );
+          this.skills.push(this.tempSkill);
+          console.log(this.skills);
+        }
+        this.tempSkill = "";
+      }
+    },
+    removeSkill(skillToBeRemoved) {
+       this.skills= this.skills.filter((skill)=>skill!==skillToBeRemoved);
+    },
+    handleSubmit(){
+        console.log('Form submitted');
+        this.passwordError = this.password <5 ?'':'Password must be at least 5 characters';
+
+        if(!this.passwordError){
+            console.log(this.email);
+            console.log(this.password);
+            console.log(this.role);
+            console.log(this.terms);
+            console.log(this.languages);
+            console.log(this.skills);
+
+
+        }
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  width: 50%;
+  padding: 20px 40px;
+  background: rgb(230, 228, 228);
+}
+</style>
